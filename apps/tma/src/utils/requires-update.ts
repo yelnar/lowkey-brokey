799fc89e911ed0ed418f7a485b78803862724eb4
Yelnar <yelnar.nauryzbayev@gmail.com>
2024-01-08 19:03:42 +0400

    fix: properly compare app versions

--- apps/tma/src/utils/requires-update.ts
+++ apps/tma/src/utils/requires-update.ts
@@ -4,8 +4,13 @@ export function requiresUpdate(currentVersion: string) {
   const current = currentVersion.split('.').map(Number)
   const target = MinVersion.split('.').map(Number)
 
+  console.log('current', current)
+  console.log('target', target)
+
   for (let i = 0; i < target.length; i++) {
-    if (Number(current[i]) < Number(target[i])) {
+    if (Number(current[i]) > Number(target[i])) {
+      return false
+    } else if (Number(current[i]) < Number(target[i])) {
       return true
     }
   }
